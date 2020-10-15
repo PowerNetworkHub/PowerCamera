115645ccc7fa6d7250743f7e2095fa19cf86e59a
Sven Arends <sven@unixguru.nl>
2020-10-15 11:24:36 +0200

    Added time parser and applied in in cmd:setduration

--- PowerCamera/src/nl/svenar/powercamera/Util.java
+++ PowerCamera/src/nl/svenar/powercamera/Util.java
@@ -1,6 +1,8 @@
 package nl.svenar.powercamera;
 
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -27,5 +29,33 @@ public class Util {
 		World world = Bukkit.getServer().getWorld(world_uid);
 
 		return new Location(world, x, y, z, yaw, pitch);
+	}
+
+	public static int timeStringToSecondsConverter(String time_input) {
+		Matcher regex_int = Pattern.compile("^\\d+[^a-zA-Z]$").matcher(time_input);
+
+		Matcher regex_seconds = Pattern.compile("\\d+[sS]").matcher(time_input);
+		Matcher regex_minutes = Pattern.compile("\\d+[mM]").matcher(time_input);
+		Matcher regex_hours = Pattern.compile("\\d+[hH]").matcher(time_input);
+
+		int seconds = 0;
+
+		if (regex_int.find()) {
+			seconds = Integer.parseInt(time_input);
+		} else {
+			if (regex_seconds.find()) {
+				seconds += Integer.parseInt(time_input.substring(regex_seconds.start(), regex_seconds.end() - 1));
+			}
+
+			if (regex_minutes.find()) {
+				seconds += Integer.parseInt(time_input.substring(regex_minutes.start(), regex_minutes.end() - 1)) * 60;
+			}
+
+			if (regex_hours.find()) {
+				seconds += Integer.parseInt(time_input.substring(regex_hours.start(), regex_hours.end() - 1)) * 3600;
+			}
+		}
+
+		return seconds;
 	}
 }
--- PowerCamera/src/nl/svenar/powercamera/commands/cmd_setduration.java
+++ PowerCamera/src/nl/svenar/powercamera/commands/cmd_setduration.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import nl.svenar.powercamera.PowerCamera;
+import nl.svenar.powercamera.Util;
 
 public class cmd_setduration extends PowerCameraCommand {
 
@@ -17,15 +18,19 @@ public class cmd_setduration extends PowerCameraCommand {
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (sender.hasPermission("powercamera.cmd.setduration")) {
 			if (args.length == 1) {
-				int duration = Integer.parseInt(args[0]);
+				int duration = Util.timeStringToSecondsConverter(args[0]);
 
-				String camera_name = plugin.player_selected_camera.get((Player) sender);
-				if (camera_name != null) {
-					plugin.getConfigCameras().setDuration(camera_name, duration);
-					sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.GREEN + "Camera path duration set to: " + duration + " seconds on camera '" + camera_name + "'");
+				if (duration > 0) {
+					String camera_name = plugin.player_selected_camera.get((Player) sender);
+					if (camera_name != null) {
+						plugin.getConfigCameras().setDuration(camera_name, duration);
+						sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.GREEN + "Camera path duration set to: " + duration + " seconds on camera '" + camera_name + "'");
+					} else {
+						sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.RED + "No camera selected!");
+						sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.GREEN + "Select a camera by doing: /" + commandLabel + " select <name>");
+					}
 				} else {
-					sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.RED + "No camera selected!");
-					sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.GREEN + "Select a camera by doing: /" + commandLabel + " select <name>");
+					sender.sendMessage(plugin.getPluginChatPrefix() + ChatColor.DARK_RED + "Duration must be greater than 0");
 				}
 
 			} else {
